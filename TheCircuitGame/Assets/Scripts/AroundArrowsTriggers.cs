3cbb41df8b71af7acb9afe937e0e31e5bcdb9712
Unknown <baqueta.dani@gmail.com>
2019-04-30 15:38:14 +0200

    Dance game arrow movement and triggers

---
+++ TheCircuitGame/Assets/Scripts/AroundArrowsTriggers.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AroundArrowsTriggers : MonoBehaviour {
+
+	public Text accuracy;
+	private void OnTriggerEnter2D(Collider2D other) {
+		accuracy.text="Good";
+		Debug.Log("Good");
+	}
+	private void OnTriggerExit2D(Collider2D other) {
+		accuracy.text="Miss";
+		Debug.Log("Miss");
+	}
+}
---
+++ TheCircuitGame/Assets/Scripts/BlackArrowTriggers.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BlackArrowTriggers : MonoBehaviour {
+
+	public Text accuracy;
+	private void OnTriggerEnter2D(Collider2D other) {
+		accuracy.text="Perfect";
+		Debug.Log("Perfect");
+	}
+	private void OnTriggerExit2D(Collider2D other) {
+		accuracy.text="Good";
+		Debug.Log("Good");
+	}
+}
