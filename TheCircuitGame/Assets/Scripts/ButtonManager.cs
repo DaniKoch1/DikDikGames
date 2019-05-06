26a1cf5ee8a74c774a7b1f0ef5133aadd7b32d76
Unknown <baqueta.dani@gmail.com>
2019-05-06 13:29:45 +0200

    Proper in game pause

--- TheCircuitGame/Assets/Scripts/ButtonManager.cs
+++ TheCircuitGame/Assets/Scripts/ButtonManager.cs
@@ -7,11 +7,22 @@ using UnityEngine.SceneManagement;
 public class ButtonManager : MonoBehaviour {
 
 	private static int missCounter;
+	private float speedOfArrows;
+	public AudioSource music;
 	public void StartGame(){
 		SceneManager.LoadScene("Dance");
 	}
 	public void PauseGame(){
 		Time.timeScale = Time.timeScale == 1.0f ? 0.0f : 1.0f;
+		if(ArrowManager.speedOfArrows == 0){
+			music.Play(0);
+			ArrowManager.speedOfArrows = speedOfArrows;
+		}
+		else{
+			music.Pause();
+			speedOfArrows = ArrowManager.speedOfArrows;
+			ArrowManager.speedOfArrows = 0;
+		}
 	}
 	public void QuitGame(){
 		#if UNITY_EDITOR
